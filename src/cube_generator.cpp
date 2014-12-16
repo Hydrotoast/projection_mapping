1d257e4968a21e12374be8dff5262cfc0c49f8a8
Gio Cielo <gccielo@gmail.com>
2014-12-16 02:47:46 -0800

    Sparse cube generation for front, top, and right faces

--- src/cube_generator.cpp
+++ src/cube_generator.cpp
@@ -8,17 +8,24 @@ int main(int argc, char *argv[]) {
   pcl::PointCloud<pcl::PointXYZ> cube; 
 
   int side = 10 + 1; 
-  int size = pow(side, 3);
+  int size = side * side * 3;
   cube.width = size; 
   cube.height = 1; 
   cube.is_dense = false;
   cube.points.resize(size);
 
   int p = 0; 
-  for (size_t i = 0; i < side; i++) 
-    for (size_t j = 0; j < side; j++) 
-      for (size_t k = 0; k < side; k++, p++) 
-        cube.points[p].getVector3fMap() = Eigen::Vector3f(i, j, k); 
+  for (size_t x = 0; x < side; x++) 
+    for (size_t y = 0; y < side; y++) 
+      cube.points[p++].getVector3fMap() = Eigen::Vector3f(x, y, 0); 
+
+  for (size_t x = 0; x < side; x++) 
+    for (size_t z = 0; z < side; z++) 
+      cube.points[p++].getVector3fMap() = Eigen::Vector3f(x, side - 1, z); 
+
+  for (size_t y = 0; y < side; y++) 
+    for (size_t z = 0; z < side; z++) 
+      cube.points[p++].getVector3fMap() = Eigen::Vector3f(side - 1, y, z); 
 
   pcl::io::savePCDFileASCII("cube.pcd", cube);
   return 0;
